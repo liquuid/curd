eeb0b5404e984dbbbfd47a83ddc3d0880578e169
Fernando Silva <fernando@liquuid.net>
2010-12-15 23:36:48 -0200

    first commit

---
+++ src/formatos.py
@@ -0,0 +1,14 @@
+formatos = [
+'Formatos'		, 'legenda'	,
+'Nokia S60/NGAGE [3gp]' , '3gp'		,
+'DV [PAL]'		, 'pal-dv'	,
+'DV [NTSC]'		, 'ntsc-dv'	,
+'DVD [PAL]'		, 'pal-dvd'	,
+'DVD [NTSC]'		, 'ntsc-dvd'	,
+'VCD [PAL]'		, 'pal-vcd'	,
+'VCD [NTSC]'		, 'ntsc-vcd'	,
+'SVCD [PAL]'		, 'pal-svcd'	,
+'SVCD [NTSC]'		, 'ntsc-svcd'	,
+'YV12 para PDP [mov]'	, 'mov'		,
+'FLV [MPG]'		, 'flvmpg'
+]
